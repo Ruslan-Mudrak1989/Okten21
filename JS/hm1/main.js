c9ad7b7af1b0ec920e82f8f9e5296730c913937d
Ruslan-Mudrak1989 <mruslan18@gmail.com>
2021-03-26 22:59:04 +0200

    done first part

---
+++ JS/hm1/main.js
@@ -0,0 +1,49 @@
+// - присвоить каждому из следующих значений свою переменную:
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false
+// Вывести каждую при помощи console.log , alert, document.write
+
+let greet = 'hello';
+let company ='owu';
+let dom = 'com';
+let uaDom = 'ua';
+let firstNum = 1;
+let decade = 10;
+let decrease = -999;
+let numHund = 123;
+let pi = 3.14;
+let float = 2.7;
+let age = 16;
+let bool = true;
+let bool2 = false;
+
+console.log(greet,company,dom,uaDom,firstNum,decade,decrease,numHund,pi,float,age,bool,bool2);
+document.write(greet,company,dom,uaDom);
+
+// - при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО.
+// Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+
+
+// let name = prompt('name');
+// let surname = prompt('surname');
+// let middle = prompt('middleName');
+
+// console.log(`${name} ${surname} ${middle}`);
+// document.write(  '<br>'+ name + ' ' + surname + ' ' + middle );
+
+
+// - Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+
+// let person = name +' '+ surname + ' ' + middle;
+// console.log(person);
+
+// при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+
+let number = +prompt();
+let number1 = +prompt();
+let number2 = +prompt();
+console.log(number,number1,number2);
+
+
+
+
+
